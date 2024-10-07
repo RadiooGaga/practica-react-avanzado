6b65f37d2e8ad0a85431d38794278cdf672518bb
mariakatt <mariahkatt@gmail.com>
2024-10-07 14:40:54 +0200

    sin useCallback

--- src/customHooks/useArithmetics.jsx
+++ src/customHooks/useArithmetics.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 
 
 export const useArithmetics = () => {
@@ -6,28 +6,28 @@ export const useArithmetics = () => {
     const [newNumber, setNewNumber] = useState(0);  // Estado para almacenar la operación 
 
     // +
-    const handleSum = useCallback((inputValue, currentValue) => {
+    const handleSum = (inputValue, currentValue) => {
         const newValue = inputValue + currentValue ; // Calcular el nuevo valor basado en el estado actual
         setNewNumber(newValue); // Actualizar el estado con el nuevo valor
         return newValue; // Retornar el nuevo valor calculado
-    },[])
+    };
 
     // -
-    const handleSubstract = useCallback((inputValue, currentValue) => {
+    const handleSubstract =(inputValue, currentValue) => {
         const newValue = inputValue - currentValue;
         setNewNumber(newValue);
         return newValue;
-    }, []);
+    };
 
     // x
-    const handleMultiply = useCallback((inputValue, currentValue) => {
+    const handleMultiply = (inputValue, currentValue) => {
         const newValue = currentValue * inputValue;
         setNewNumber(newValue);
         return newValue;
-    }, []);
+    };
 
     // :
-    const handleDivide = useCallback((inputValue, currentValue) => {
+    const handleDivide = (inputValue, currentValue) => {
         if (inputValue === 0) {
             alert('No se puede dividir por 0');
             return newNumber;
@@ -35,14 +35,14 @@ export const useArithmetics = () => {
         const newValue = inputValue / currentValue;
         setNewNumber(newValue);
         return newValue;
-    });
+    };
 
     // %
-    const handlePercentage = useCallback((inputValue, currentValue) => {
+    const handlePercentage = (inputValue, currentValue) => {
         const newValue = inputValue * ( currentValue / 100);
         setNewNumber(newValue);
         return newValue;
-    });
+    };
 
     return { handleSum, handleSubstract, handleMultiply, handleDivide, handlePercentage };
 };
